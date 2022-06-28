b23725d3bd26d77309006a6c0f445274a8cb3dd7
Michelle DeMars <thenotfrenchgirl@gmail.com>
2022-06-27 21:01:08 -0500

    adding jsondata

---
+++ Mapping_GeoJSON_Points/js/logic.js
@@ -0,0 +1,69 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup();
+   }
+}).addTo(map);
+
+
+
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "streets-v11",
+  accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+// L.circle([34.0522, -118.2437], {
+//  color: "black",
+//  fillColor: 'lightyellow',
+//  radius: 300
+// }).addTo(map);
+
+// Get data from cities.js
+//let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+//cityData.forEach(function(city) {
+//  console.log(city)
+//  L.circleMarker(city.location, {
+//    color: "orange",
+//    fillColor: 'orange',
+//    radius: city.population/100000
+//  })
+//  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//.addTo(map);
+// });
