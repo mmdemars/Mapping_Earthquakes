70919526992b2105fa6696c1b85b4cbae4ddf3a8
Michelle DeMars <thenotfrenchgirl@gmail.com>
2022-07-01 19:06:09 -0500

    final polygon code practice

--- Mapping_GeoJSON_Polygons/js/logica.js
+++ Mapping_GeoJSON_Polygons/js/logica.js
@@ -43,29 +43,30 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [43.7, -79.3],
   zoom:11,
-  layers: [satelliteStreets]
+  layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/mmdemars/Mapping_Earthquakes/main/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/mmdemars/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
+  color: 'blue',
+  fillColor: 'yellow',
   weight: 2
 } 
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data, {
   style: myStyle,
   onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+    layer.bindPopup("<h3> Neighborhood: </h3>" + feature.properties.AREA_NAME);
   }
 })
 .addTo(map);
